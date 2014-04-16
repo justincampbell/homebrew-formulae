64011092c8ff3858ef2d3fdd44f373564bafda1c
Justin Campbell <justin@justincampbell.me>
2014-04-16 00:19:07 -0400

    rr v0.2.2

--- Formula/rr.rb
+++ Formula/rr.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Rr < Formula
   homepage 'https://github.com/justincampbell/rr'
-  url 'https://github.com/justincampbell/rr/archive/v0.2.1.tar.gz'
-  sha1 'b0b5243033e56f701c27bf7701e43a506c32c3fc'
+  url 'https://github.com/justincampbell/rr/archive/v0.2.2.tar.gz'
+  sha1 'c0ef9ffde659506a083444f30cb996ddfea405a2'
 
   head 'https://github.com/justincampbell/rr.git'
 
