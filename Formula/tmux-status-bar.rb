411e57450f0f9db5587a9ba240a9e7199b04a01d
Justin Campbell <justin@justincampbell.me>
2014-05-15 13:39:43 -0400

    tmux-status-bar v0.2.0

--- Formula/tmux-status-bar.rb
+++ Formula/tmux-status-bar.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class TmuxStatusBar < Formula
   homepage 'https://github.com/justincampbell/tmux-status-bar'
-  url 'https://github.com/justincampbell/tmux-status-bar/archive/v0.1.0.tar.gz'
-  sha1 '32eb8d159c7873e34fa2abb9cc45c6b817a9f296'
+  url 'https://github.com/justincampbell/tmux-status-bar/archive/v0.2.0.tar.gz'
+  sha1 '2fcd7cc592abf420d2bab8c7ff92560d41b0505f'
 
   head 'https://github.com/justincampbell/tmux-status-bar.git'
 
