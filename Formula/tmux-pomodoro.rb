ede53455eedb267b5b2f2042060beb6903e16878
Justin Campbell <justin@justincampbell.me>
2014-08-01 13:37:20 -0400

    tmux-pomodoro v1.1.1

--- Formula/tmux-pomodoro.rb
+++ Formula/tmux-pomodoro.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class TmuxPomodoro < Formula
   homepage 'https://github.com/justincampbell/tmux-pomodoro'
-  url 'https://github.com/justincampbell/tmux-pomodoro/archive/v1.1.0.tar.gz'
-  sha1 '327b44ae3f711bd213866dacfb5cedd8b69be454'
+  url 'https://github.com/justincampbell/tmux-pomodoro/archive/v1.1.1.tar.gz'
+  sha1 'cad7586ef263629df1e27cade690277244a085dc'
   head 'https://github.com/justincampbell/tmux-pomodoro.git'
 
   depends_on 'git' => :build
