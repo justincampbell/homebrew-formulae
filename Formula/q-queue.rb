ec104dbc286c5ac33783abed0863af211091f8c7
Justin Campbell <justin@justincampbell.me>
2014-08-21 09:27:35 -0400

    Fix q-queue constant capitalization

--- Formula/q-queue.rb
+++ Formula/q-queue.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class Qqueue < Formula
+class QQueue < Formula
   homepage 'https://github.com/justincampbell/q-queue'
   url 'https://github.com/justincampbell/q-queue/archive/v0.1.1.tar.gz'
   sha1 '92567c5c4a472dad3139a14cd35b9679d68f5746'
