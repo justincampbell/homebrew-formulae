33bca826e1a2681bd36a805336e2e3483044d160
Justin Campbell <justin@justincampbell.me>
2014-08-25 19:06:19 -0400

    q-queue v0.2.1

--- Formula/q-queue.rb
+++ Formula/q-queue.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class QQueue < Formula
   homepage 'https://github.com/justincampbell/q-queue'
-  url 'https://github.com/justincampbell/q-queue/archive/v0.2.0.tar.gz'
-  sha1 '0a77099cd91edeb3049a889c7d34b3ac34008b5e'
+  url 'https://github.com/justincampbell/q-queue/archive/v0.2.1.tar.gz'
+  sha1 '7a3ef61af8dc9082fe9c96ebe4b672bf953b17d6'
 
   head 'https://github.com/justincampbell/q-queue.git'
 
